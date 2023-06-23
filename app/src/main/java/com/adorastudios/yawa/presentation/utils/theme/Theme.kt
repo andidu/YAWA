278f36e4f5fc5390ca7df5831a68b77d417ac586
andidu <andiduorange@gmail.com>
2023-06-23 19:37:06 +0300

    remove unnecessary sdk check

--- app/src/main/java/com/adorastudios/yawa/presentation/utils/theme/Theme.kt
+++ app/src/main/java/com/adorastudios/yawa/presentation/utils/theme/Theme.kt
@@ -1,7 +1,6 @@
 package com.adorastudios.yawa.presentation.utils.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
@@ -35,7 +34,7 @@ fun YAWATheme(
     content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
